f807f64210124bba1a7ac7b410f3f9d26357ce01
cjf <1227786808@qq.com>
2022-09-11 21:15:18 +0800

    hot-fix commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,8 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git hot-fix");
         System.out.println("hello git master");
+        System.out.println("hello git master");
+        System.out.println("hello git master");
+        System.out.println("hello git master");
     }
 }
