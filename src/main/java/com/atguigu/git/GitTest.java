30259db2fd1a4758f176300b0ca7837bdb5cb04b
startmyjourney <113274987+startmyjourney@users.noreply.github.com>
2022-09-11 21:17:25 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("hello git master");
         System.out.println("hello git master");
         System.out.println("hello git master");
-        System.out.println("hello git master");
+        System.out.println("hello git master1234");
     }
 }
